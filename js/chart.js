7dbfa68821e8e896155dfc7305e25fc4b81508f4
riconogu <65628216+riconogu@users.noreply.github.com>
2022-04-08 10:51:09 -0400

    new

--- js/chart.js
+++ js/chart.js
@@ -11,15 +11,15 @@
     drawGrid(width, height, 40, padding, 'white', 1);
     drawGrid(width, height, 2, padding, 'black', 2);
     var labels = [
-      { text: 'Memoria Trájica', horizontal: 'left', vertical: 'top', x: width/ 6, y: height/ 8 },
+      { text: 'Memoria Trájica', horizontal: 'left', vertical: 'top', x: width/ 8, y: height/ 10 },
       { text: '', horizontal: 'center',vertical: 'top', x: width / 2, y: 0 },
-      { text: 'Memoria insurgente', horizontal: 'right', vertical: 'top', x: width/ 1.2, y: height/ 8 },
+      { text: 'Memoria insurgente', horizontal: 'right', vertical: 'top', x: width/ 1.18, y: height/ 10 },
       { text: '', horizontal: 'left', vertical: 'middle', x: 0, y: height / 2 },
       // { text: 'Centerism', horizontal: 'center', vertical: 'middle', x: width / 2, y: height / 2 },
       { text: '', horizontal: 'right', vertical: 'middle', x: width, y: height / 2 },
-      { text: 'Memoria indiferente', horizontal: 'left', vertical: 'bottom', x: width/ 6, y: height/ 1.2 },
+      { text: 'Memoria indiferente', horizontal: 'left', vertical: 'bottom', x: width/ 8, y: height/ 1.18 },
       { text: '', horizontal: 'center', vertical: 'bottom', x: width / 2, y: height },
-      { text: 'Memoria heróica', horizontal: 'right', vertical: 'bottom', x: width/ 1.2, y: height/ 1.2 },
+      { text: 'Memoria heróica', horizontal: 'right', vertical: 'bottom', x: width/ 1.18, y: height/ 1.15 },
     ];
     labels.forEach(function(label) {
       drawText(label, 10);
